ef046219319c57bc75f2e3629baa219a3a83ea45
leonardoGpaulo <lgualbertodepaulo@gmail.com>
2026-04-06 16:44:10 -0300

    Commit inicial

---
+++ revisao/revisao.js
@@ -0,0 +1,22 @@
+function revisao(){
+    let nome = "João";
+    console.log(nome);
+    if (true){
+        let sobrenome = "Roccella";
+        console.log(sobrenome);
+    }
+    //console.log(sobrenome); // variáveç sobrenome não existe nesse contexto
+    const idade = 40;
+    console.log(idade);
+    //idade = 41;
+    
+
+    const precoOriginal = 100;
+    const desconto = 20;
+    let precoFinal = precoOriginal - desconto;
+
+    console.log("Preço original: " + precoOriginal);
+    console.log("Desconto: " + desconto);
+    console.log("Preço Final: " + precoFinal);
+}
+revisao();

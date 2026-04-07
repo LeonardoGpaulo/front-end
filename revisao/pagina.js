8e643f949d1c6404f8f1b58c27b11af197008cb5
leonardoGpaulo <lgualbertodepaulo@gmail.com>
2026-04-07 16:18:51 -0300

    fix: corrigindo a maneira de fazero HTML em JavaScript

--- revisao/pagina.js
+++ revisao/pagina.js
@@ -1,10 +1,6 @@
 function pagina(){
-
-    const body = document.body
-
-    const div = document.createElement("div")
-    div.classList.add("container")
-    body.appendChild(div)
+    
+    const div = document.querySelector("#container")
 
     const titulo = document.createElement("h2");
     titulo.textContent = "Lista de tarefas"

37c0725ed845b8a0ecbbc174723fde1726024269
leonardoGpaulo <lgualbertodepaulo@gmail.com>
2026-04-07 15:51:43 -0300

    feat:adicionando as funcionalidades de criar elementos HTML em JavaScript

---
+++ revisao/pagina.js
@@ -0,0 +1,59 @@
+function pagina(){
+
+    const body = document.body
+
+    const div = document.createElement("div")
+    div.classList.add("container")
+    body.appendChild(div)
+
+    const titulo = document.createElement("h2");
+    titulo.textContent = "Lista de tarefas"
+    div.appendChild(titulo)
+    
+    const input = document.createElement("input");
+    input.type = "text";
+    input.id = "tarefaInput";
+    input.placeholder = "digite sua tarefa";
+    div.appendChild(input)
+
+    const botao = document.createElement("button");
+    botao.id = "btnAdicionar";
+    botao.textContent = "adicionar"
+    div.appendChild(botao) 
+
+    const lista = document.createElement("listaTarefas");
+    lista.id = "listaTarefas"
+    div.appendChild(lista)
+
+    botao.addEventListener("click", ()=>{
+        //Criar novo elemento da lista
+        const novaTarefa = document.createElement("li");
+        const texto = input.value.trim();
+
+        if(texto === ""){
+        alert("digite algo!");
+        return;
+    }
+
+    novaTarefa.textContent = texto;//adiciona texto ao elemento da lista
+    novaTarefa.classList.add('destaque'); //adiciona a classe "destaque" ao li
+    novaTarefa.setAttribute("data-tarefa", texto.toLowerCase()); //adiciona atributos personalizados
+    novaTarefa.style.color = "#333"; //muda a cor do texto
+
+    lista.appendChild(novaTarefa); //adiciona elemento
+
+    //evento para remover item ao clicar
+    novaTarefa.addEventListener("click", () => {
+        const tarefa = novaTarefa.getAttribute("data-tarefa");
+        console.log("Removendo: ", tarefa);
+        novaTarefa.classList.remove("destaque"); //remove a classe html destaque
+        novaTarefa.classList.add("removido"); //acrescenta a class html removido
+        novaTarefa.innerHTML += " (removida)"; //acrescenta o texto (removida) ao texto da tarefa
+        setTimeout(()=>{lista.removeChild(novaTarefa);}, 1000); //remove tarefa depois de 1 segundo
+    });
+
+    });
+
+    
+}
+
